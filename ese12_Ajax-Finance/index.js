8fc0469c89ab6bfa3514dedcf4bdd9ec20392dbd
LookAtOmkar <61886825+LookAtOmkar@users.noreply.github.com>
2020-05-12 02:20:09 +0200

    Update Project
    Added some code to progress, to go on with the project.

--- ese12_Ajax-Finance/index.js
+++ ese12_Ajax-Finance/index.js
@@ -26,20 +26,34 @@ let  companies=[
 ];
 
 
-
 $(document).ready(function () {
+    let _Sector=$("#ComboSector");
+    let  ctx;
 
-    //carico Combobox
-    for(let i=0;i<companies.length;i++)
-    {
-        $("<option>").prop("value",companies[i].Id).text(companies[i].Name).appendTo($("#Lst_Companies"));
-    }
+    //----------------------------CARICAMENTO COMBOBOX COMPANIES TRADIZIONALE---------------------------------------
     
-    //RICERCA INCREMENTALE
+    //for(let i=0;i<companies.length;i++)
+    //{
+    //    $("<option>").prop("value",companies[i].Id).text(companies[i].Name).appendTo($("#Lst_Companies"));
+    //}
+
+    //---------------------CARICAMENTO COMBOBOX COMPANIES TRAMITE Node.js--> json-server-----------------------------
+    
+    let _companies= $.getJSON("http://localhost:3000/companies",function(data){
+        console.log(data);
+        for(let i=0;i<data.length;i++)
+        {
+            $("<option>").prop("value",data[i].Id).text(data[i].Name).appendTo($("#Lst_Companies"));
+        }
+    });
+
+
+    
+    //--------------------------------RICERCA INCREMENTALE-------------------------------------------
     $("#txtSearch").on("keyup",function(){
-        if($(this).value.length>=2)
+        if($(this).val().length>=2)
             {
-                let Ricerca=inviaRichiesta("GET","https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + $(this).value+ "&apikey=4OGSYZ0P5TW95U2Z");
+                let Ricerca=inviaRichiesta("GET","https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + $(this).val()+ "&apikey=4OGSYZ0P5TW95U2Z");
                 Ricerca.done(function(data){
                     $(_table).html("");
                     //alert("ok");
@@ -52,6 +66,7 @@ $(document).ready(function () {
                             let Details=inviaRichiesta("GET","https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + symbol + "&apikey=4OGSYZ0P5TW95U2Z",{},false);
                             Details.done(function(data){
                                 //alert("Corretto Dati");
+                                console.log(data);
                                 caricaTabella(data["Global Quote"]);
                             }) 
                             Details.fail(error);
@@ -61,33 +76,90 @@ $(document).ready(function () {
                             // })
                         }
                     }
-                    catch(ee){};
+                    catch(error){};
                 });
                 Ricerca.fail(error);
             }
     });
 
-    //RICERCA TRAMITE COMBOBOX
+    //---------------------------------------RICERCA TRAMITE COMBOBOX---------------------------------
     $("#Lst_Companies").on("change",function(){
         let symbol=$(this).prop("value");
         getGlobalQuotes(symbol);
     });
 
-    //CHART
-    $.getJSON("http://localhost:3000/chart", function (data) {
-       var ctx = document.getElementById('myChart').getContext('2d');
-       var myChart = new Chart(ctx, data);
-   })
-    //getGlobalQuotes("IBM");
+    //--------------------------------------CARICAMENTO COMBOBOX SECTOR------------------------------
+    let RichiediSector=$.getJSON("http://localhost:3000/SECTOR",function(data){
+        console.log(data);
+        for(let key in data)
+        {
+            $("<option>").prop("value",key).text(key).appendTo(_Sector);
+        }    
+    
+    });
+
+    //-----------------------------------------------CHART--------------------------------------------
+
+    ///Default grafico
+    //$.getJSON("http://localhost:3000/chart", function (data) {
+    //   let  ctx = document.getElementById('myChart').getContext('2d');
+    //   let myChart = new Chart(ctx, data);
+    //});
+
+    Chart.defaults.global.elements.rectangle.borderWidth = 5;
+
+    _Sector.on("change",function(){
+        let Sector_Selected=$(this).val();
+        let Details=inviaRichiesta("GET","http://localhost:3000/SECTOR",Sector_Selected);
+        Details.done(function(data){
+            UpdateGraphic(data[Sector_Selected]); 
+        });
+
+        Details.fail(error);
+
+    });
+
+    //-------------------------------------DOWNLOAD-------------------------------------------
+    $("<input>").prop({
+        "type":"button",
+        "value":"DOWNLOAD",
+        "id":"Download"
+    }).addClass("btn btn-success").appendTo($("#Options"));
+    $("#Options #Download").on("click",function(){
+        ///DOWNLOAD DEL GRAFICO E SALVARLO IN UN FILE 
+    })
+
 });
 
+function UpdateGraphic(DataChart){
+    let  ctx = document.getElementById('myChart').getContext('2d');
+    let _Chart_Details=$.getJSON("http://localhost:3000/chart",function(chart){
+        chart["data"]["labels"]=[];
+        chart["data"]["datasets"]["data"]=[];
+
+        let DataSets=chart["data"]["datasets"];
+        let Chart_Data= chart["data"];
+
+        for(let key in DataChart)
+        {
+            Chart_Data["labels"].push(key);
+            DataSets["data"].push(DataChart[key]);
+        }
+        let Graphic=new Chart(ctx,chart);
+    });   
+}
+
+
+
 function caricaTabella(data){
-    let _tr=$("<tr>");
+    let _tr=$("<tr>").css({
+        "border":"1px double black"
+    });
     for(let key in data) //key in questo caso sarebbe un dato di global quote come "01. symbol","02. open"
     {
-        $("<td>").text(data[key]).appendTo(_tr);
+        $("<td>").addClass("td").text(data[key]).appendTo(_tr);
     }
-    $(_table).append(_tr);
+    _tr.appendTo(_table);
 }
 
 
